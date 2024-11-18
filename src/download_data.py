625846dc66da9eb0d79773265896e1143c34ef38
Lucas <lucasrs202@gmail.com>
2024-11-18 19:37:31 -0300

    Remove unnecessary comments

--- src/download_data.py
+++ src/download_data.py
@@ -2,17 +2,14 @@ import kagglehub
 import shutil
 import os
 
-# Download latest version
 path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
 
 print("Path to dataset files:", path)
-# Define the destination directory
+
 destination_dir = './data'
 
-# Ensure the destination directory exists
 os.makedirs(destination_dir, exist_ok=True)
 
-# Move the downloaded files to the destination directory
 for file_name in os.listdir(path):
     full_file_name = os.path.join(path, file_name)
     if os.path.isfile(full_file_name):
